c18b1319605244d6062ee899cee2ffcacdbd3e96
xmanrui <841206367@qq.com>
2017-11-13 23:59:12 +0800

    PEP8修改

--- love2.py
+++ love2.py
@@ -4,10 +4,8 @@ import numpy as np
 
 def draw_love():
     x = np.linspace(0, 2, 1000)
-    f = (np.power(x, 2/3) + np.sqrt(np.power(x, 4/3)
-        - 4 * np.power(x, 2) + 4)) / 2
-    g = (np.power(x, 2/3) - np.sqrt(np.power(x, 4/3)
-        - 4 * np.power(x, 2) + 4)) / 2
+    f = (np.power(x, 2/3) + np.sqrt(np.power(x, 4/3) - 4 * np.power(x, 2) + 4)) / 2
+    g = (np.power(x, 2/3) - np.sqrt(np.power(x, 4/3) - 4 * np.power(x, 2) + 4)) / 2
 
     plt.plot(x, f, color='red', linewidth=2)
     plt.plot(-x, f, color='red', linewidth=2)

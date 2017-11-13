46dc2207169c3ea13ceb72764e5f0dff9cd7676b
xmanrui <841206367@qq.com>
2017-11-14 00:02:54 +0800

    修改变量名

--- love4.py
+++ love4.py
@@ -7,12 +7,12 @@ def draw_love():
     x = np.linspace(0, math.pi/3, 1000)
     f = -1 * np.tan(np.sqrt(1 - np.power(np.abs(x), 3/2))) \
         + math.pi / 2
-    h = np.sqrt(1 / 4 - np.power(x - 1 / 2, 2)) + math.pi / 2
+    g = np.sqrt(1 / 4 - np.power(x - 1 / 2, 2)) + math.pi / 2
 
     plt.plot(x, f, color='red', linewidth=2)
     plt.plot(-x, f, color='red', linewidth=2)
-    plt.plot(x, h, color='red', linewidth=2)
-    plt.plot(-x, h, color='red', linewidth=2)
+    plt.plot(x, g, color='red', linewidth=2)
+    plt.plot(-x, g, color='red', linewidth=2)
 
     plt.ylim(-1, 3)
     plt.xlim(-2, 2)

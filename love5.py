32a5619271720711d092b2ba8f5c6fdb15834eba
xmanrui <841206367@qq.com>
2017-11-14 00:31:05 +0800

    修改变量

--- love5.py
+++ love5.py
@@ -3,17 +3,16 @@ import numpy as np
 
 
 def love5():
-
     x = np.linspace(0, 2, 1000)
-    g = -1 * np.sqrt(1 - 1.65 * np.arctan(x) * np.arctan(x))
-    f = np.sqrt(1 - np.power(np.power(np.abs(x), np.abs(x)), 2))
+    f = np.sqrt(1 - np.power(np.power(np.abs(x), np.abs(x)), 2)) + 2
+    g = -1 * np.sqrt(1 - 1.65 * np.arctan(x) * np.arctan(x)) + 2
 
     plt.plot(x, g, color='red', linewidth=2)
     plt.plot(-x, g, color='red', linewidth=2)
     plt.plot(x, f, color='red', linewidth=2)
     plt.plot(-x, f, color='red', linewidth=2)
 
-    plt.ylim(-2, 2)
+    plt.ylim(0, 4)
     plt.xlim(-2, 2)
     plt.title('LOVE')
     plt.legend()

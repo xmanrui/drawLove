720ac22b092502b9a07879ea811f383472e5919f
xmanrui <841206367@qq.com>
2017-11-13 23:51:34 +0800

    去掉多余的代码

--- love2.py
+++ love2.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 
 def draw_love():
     x = np.linspace(0, 2, 1000)
     y1 = (np.power(x, 2/3) + np.sqrt(np.power(x, 4/3)
         - 4 * np.power(x, 2) + 4)) / 2
-    y2 = (np.power(x, 2 / 3) - np.sqrt(np.power(x, 4 / 3)
+    y2 = (np.power(x, 2/3) - np.sqrt(np.power(x, 4/3)
         - 4 * np.power(x, 2) + 4)) / 2
+
     plt.plot(x, y1, color='red', linewidth=2)
     plt.plot(-x, y1, color='red', linewidth=2)
     plt.plot(x, y2, color='red', linewidth=2)

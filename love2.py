7c7a40700786a64b9b88bb5eb33014c621693deb
xmanrui <841206367@qq.com>
2017-11-13 23:56:19 +0800

    修改变量名

--- love2.py
+++ love2.py
@@ -3,16 +3,16 @@ import numpy as np
 
 
 def draw_love():
-    x = np.linspace(0, 2, 1000)
-    y1 = (np.power(x, 2/3) + np.sqrt(np.power(x, 4/3)
+    x = np.linspace(-2, 2, 1000)
+    f = (np.power(x, 2/3) + np.sqrt(np.power(x, 4/3)
         - 4 * np.power(x, 2) + 4)) / 2
-    y2 = (np.power(x, 2/3) - np.sqrt(np.power(x, 4/3)
+    g = (np.power(x, 2/3) - np.sqrt(np.power(x, 4/3)
         - 4 * np.power(x, 2) + 4)) / 2
 
-    plt.plot(x, y1, color='red', linewidth=2)
-    plt.plot(-x, y1, color='red', linewidth=2)
-    plt.plot(x, y2, color='red', linewidth=2)
-    plt.plot(-x, y2, color='red', linewidth=2)
+    plt.plot(x, f, color='red', linewidth=2)
+    plt.plot(-x, f, color='red', linewidth=2)
+    plt.plot(x, g, color='red', linewidth=2)
+    plt.plot(-x, g, color='red', linewidth=2)
 
     plt.ylim(-2, 2)
     plt.xlim(-2, 2)
